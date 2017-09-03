9e2176accf15f315f3cff944c8ddc29348057fe0
Allvaro Barbero <albarjip@gmail.com>
2017-09-03 22:54:12 +0200

    Allow to run several trainings in parallel
    Added missing dependency

--- runbatch.sh
+++ runbatch.sh
@@ -10,16 +10,18 @@ MATPLOTLIBCONF='import matplotlib as mpl\nmpl.use("Agg")\n'
 
 notebook=$1
 
+generatedscript=$(mktemp -p .)
+
 # Transform to python code
 jupyter nbconvert --to script --stdout $notebook |
 # Remove ipython magics
 grep -v 'get_ipython().magic' |
 # Nullify matplotlib
-cat <(echo -e "$MATPLOTLIBCONF") - > .generatedscript.py
+cat <(echo -e "$MATPLOTLIBCONF") - > $generatedscript
 
 # Run script
-python .generatedscript.py
+python $generatedscript
 
 # Delete script
-rm .generatedscript.py
+rm $generatedscript
 
