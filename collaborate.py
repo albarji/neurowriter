6cadc64110a570bf68bc7e88864e6c551f216495
Álvaro Barbero Jiménez <albarjip@gmail.com>
2018-12-18 18:35:47 +0100

    Style corrections

--- collaborate.py
+++ collaborate.py
@@ -1,5 +1,6 @@
 # Human-machine collaborative text generation using a pre-trained model
-# Alternates between asking the human for input and asking the model to generate, producing a collaborative composition
+# Alternates between asking the human for input and asking the model to
+# generate, producing a collaborative composition
 
 import argparse
 from keras.models import load_model
@@ -17,13 +18,14 @@ def collaborate(modelname, encodername, creativity):
     model = load_model(modelname)
 
     # Text generation loop
-    writer = Writer(model, encoder, creativity=creativity, batchsize=1, beamsize=1)
+    writer = Writer(model, encoder, creativity=creativity, batchsize=1,
+                    beamsize=1)
     while True:
         collaborate_document(writer)
 
 
 def collaborate_document(writer, maxlines=14):
-    """Creates a single document by collaboration between human and pre-trained model
+    """Creates a single document by collaboration between human and model
 
     Inputs
         writer: Writer object for automated text generation
@@ -48,19 +50,28 @@ def collaborate_document(writer, maxlines=14):
         lines += 1
 
     print("\n\n")
-    print("Human, we created this composition together. Thanks for your inspiration!\n")
+    print("Human, we created this composition together. "
+          "Thanks for your inspiration!\n")
     print(composition)
     return composition
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Human-machine collaborative text generation, following the style of'
-                                                 ' a pre-trained style model.')
+    parser = argparse.ArgumentParser(
+        description='Human-machine collaborative text generation, '
+                    'following the style of a pre-trained style model.')
     parser.add_argument('model', type=str, help='name of the pre-trained model')
-    parser.add_argument('encoder', type=str, help='name of the pre-trained encoder')
-    parser.add_argument('--seed', type=str, help='seed to use to initialize the generator. Default: empty string',
+    parser.add_argument('encoder', type=str,
+                        help='name of the pre-trained encoder')
+    parser.add_argument('--seed', type=str,
+                        help='seed to use to initialize the generator. '
+                             'Default: empty string',
                         default='')
-    parser.add_argument('--creativity', type=float, help='amount of creativity in the generation. Default: 0.5',
+    parser.add_argument('--creativity', type=float,
+                        help='amount of creativity in the generation. '
+                             'Default: 0.5',
                         default=0.5)
     args = parser.parse_args()
 
-    collaborate(modelname=args.model, encodername=args.encoder, creativity=args.creativity)
+    collaborate(modelname=args.model, encodername=args.encoder,
+                creativity=args.creativity)
